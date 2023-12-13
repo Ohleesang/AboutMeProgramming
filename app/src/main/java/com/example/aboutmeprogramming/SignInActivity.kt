7c66ab17cde82527e6451ce5947965929f4d9146
Ohleesang <jammmmed@gmail.com>
2023-12-13 17:39:43 +0900

    Feat : Button 기능 추가
    btn_login : editText 의 두 값이 비어있지 않을경우, 클릭할 때 HomeActivity 으로 이동 + Intent에 ID값을 넣어서 전달
    btn_signUp : 클릭할 때,SignUpActivity 으로 이동

--- app/src/main/java/com/example/aboutmeprogramming/SignInActivity.kt
+++ app/src/main/java/com/example/aboutmeprogramming/SignInActivity.kt
@@ -1,11 +1,46 @@
 package com.example.aboutmeprogramming
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_login = findViewById<Button>(R.id.btn_login)
+        val btn_signUp = findViewById<Button>(R.id.btn_signUp)
+        val id = findViewById<EditText>(R.id.editText_id)
+        val pw = findViewById<EditText>(R.id.editText_pw)
+
+
+        //로그인 버튼 눌렸을때
+        btn_login.setOnClickListener {
+            var intent = Intent(this, HomeActivity::class.java)
+            var inputId: String?
+            var inputPw: String?
+            inputId = id.text.toString()
+            inputPw = pw.text.toString()
+            if (inputId == "" || inputPw == "") {
+                // 만약, 두값이 입력되지 않는다면 return
+                Toast.makeText(this,"아이디/비밀번호를 확인해주세요",Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            intent.putExtra("id", inputId)
+            startActivity(intent) // HomeActivity 실행!
+            Toast.makeText(this,"로그인 성공!",Toast.LENGTH_SHORT).show()
+        }
+
+
+        //회원가입 버튼 눌렸을때
+        btn_signUp.setOnClickListener {
+            var intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this,"회원가입 액티비티!",Toast.LENGTH_SHORT).show()
+        }
     }
 }
