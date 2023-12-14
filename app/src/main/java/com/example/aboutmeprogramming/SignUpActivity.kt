8ddf597a669e6c9c615a42e5e92ae70ab0de627c
Ohleesang <jammmmed@gmail.com>
2023-12-14 11:43:03 +0900

    Feat : SignUpActivity 의 Button 기능 추가
    btn_signUp : editText의 값이 모두 입력될 경우에 클릭 시, 액티비티 종료
    만약, 하나라도 값이 없다면 Toast 메세지 출력 후 그대로 유지

--- app/src/main/java/com/example/aboutmeprogramming/SignUpActivity.kt
+++ app/src/main/java/com/example/aboutmeprogramming/SignUpActivity.kt
@@ -2,10 +2,44 @@ package com.example.aboutmeprogramming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val btn_signUp = findViewById<Button>(R.id.btn_signUp)
+
+        val name = findViewById<EditText>(R.id.editText_name)
+        val id = findViewById<EditText>(R.id.editText_id)
+        val pw = findViewById<EditText>(R.id.editText_pw)
+
+        //회원가입 버튼을 눌렸을때
+        btn_signUp.setOnClickListener {
+
+            var inputName: String?
+            var inputId: String?
+            var inputPw: String?
+
+            inputName = name.text.toString()
+            inputId = id.text.toString()
+            inputPw = pw.text.toString()
+
+            val isEmpty = (inputName == "" || inputId == "" || inputPw == "")
+
+            if (isEmpty) {
+                Toast.makeText(this, "입력되지 않은 정보가 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+
+            //종료!
+            Toast.makeText(this, "회원가입 완료!", Toast.LENGTH_SHORT).show()
+
+            finish()
+        }
     }
 }
