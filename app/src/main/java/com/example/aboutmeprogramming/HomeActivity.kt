881c069cb0db2f6f6bc58640dafcb46313c259e8
Ohleesang <jammmmed@gmail.com>
2023-12-14 16:40:02 +0900

    Feat : randImageView() add
    randImageView() : 레이아웃에 있는 ImageView를 랜덤으로 5개의 사진으로 변경

--- app/src/main/java/com/example/aboutmeprogramming/HomeActivity.kt
+++ app/src/main/java/com/example/aboutmeprogramming/HomeActivity.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import kotlin.random.Random
 
 class HomeActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -14,6 +16,7 @@ class HomeActivity : AppCompatActivity() {
 
         val btn_exit = findViewById<Button>(R.id.btn_exit)
 
+        randImageView()
         //버튼 기능
         btn_exit.setOnClickListener {
             finish()
@@ -33,5 +36,17 @@ class HomeActivity : AppCompatActivity() {
     fun setTextTextView(resource: Int, str: String) {
         val textView = findViewById<TextView>(resource)
         textView.setText(textView.text.toString() + " ${str}")
+    }
+    //ImageView 랜덤으로 지정하기
+    fun randImageView(){
+        val randInt = Random.nextInt(5) //랜덤으로 0~4까지 값주기
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        when(randInt){
+            0 -> imageView.setImageResource(R.drawable.duck_3)
+            1 -> imageView.setImageResource(R.drawable.duck_4)
+            2 -> imageView.setImageResource(R.drawable.duck_5)
+            3 -> imageView.setImageResource(R.drawable.duck_6)
+            4 -> imageView.setImageResource(R.drawable.duck_7)
+        }
     }
 }
