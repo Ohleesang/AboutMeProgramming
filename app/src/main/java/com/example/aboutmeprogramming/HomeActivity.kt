2ef35fc4f61f097edd83072d01ab32ffb8d735fa
Ohleesang <jammmmed@gmail.com>
2023-12-14 12:18:00 +0900

    Feat : HomeActivity 의 Button 기능 추가, 코드로 TextView 문자열 추가
    btn_exit : 실행되고있는 액티비티를 종료.
    
    intent 에서 값을 받아, TextView_id 의 Widget의 문자열로 추가
    그 이외의 문자열은 setTextTextView(Int,String) 라는 함수를 생성하여 추가

--- app/src/main/java/com/example/aboutmeprogramming/HomeActivity.kt
+++ app/src/main/java/com/example/aboutmeprogramming/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.example.aboutmeprogramming
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
@@ -11,9 +12,30 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
+        val btn_exit = findViewById<Button>(R.id.btn_exit)
+
         //인텐드에서 값을 받는다.
         val strData = intent.getStringExtra("id")
-//        val textView = findViewById<TextView>(R.id.textView_id_homeActivity)
-//        textView.setText(strData)
+        val idTextView = findViewById<TextView>(R.id.textView_id)
+
+        //받은 값을 입력
+        idTextView.setText(idTextView.text.toString() + " ${strData}")
+
+        //버튼 기능
+        btn_exit.setOnClickListener {
+            finish()
+        }
+
+        //이름 나이 MBTI 입력하기
+        setTextTextView(R.id.textView_name, "이상오")
+        setTextTextView(R.id.textView_age, "30")
+        setTextTextView(R.id.textView_mbti, "INFP")
+
+    }
+
+    //TextView 의 문자열 값을 추가 하기
+    fun setTextTextView(resource: Int, str: String) {
+        val textView = findViewById<TextView>(resource)
+        textView.setText(textView.text.toString() + " ${str}")
     }
 }
